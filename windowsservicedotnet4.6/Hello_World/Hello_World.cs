525406216d4500510588e8197adb65dac00bc887
Guthireddy Reddy <guthireddy.reddy@mphasis.com>
2022-08-31 15:35:52 +0530

    First Commit

---
+++ windowsservicedotnet4.6/Hello_World/Hello_World.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    public partial class Hello_World : ServiceBase
+    {
+        public Hello_World()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            using (StreamWriter writeText = new StreamWriter("c://Temp//output.txt"))
+            {
+                writeText.WriteLine("Hello World...");
+            }
+        }
+
+        protected override void OnStop()
+        {
+
+        }
+    }
+}
