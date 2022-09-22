e3ec571606bf2a0add28c9b44c852e40a9e0918c
Guthireddy Reddy <guthireddy.reddy@mphasis.com>
2022-09-22 14:44:10 +0530

    Withoutunit test cases

---
+++ withoutunittestcase/WithoutUnittestcases/Crudwebform.aspx.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloApp
+{
+    public partial class Crudwebform : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DefaultEmpRecord();
+            }
+        }
+        private void DefaultEmpRecord()
+        {
+            //creating dataTable   
+            DataTable dt = new DataTable();
+            DataRow dr;
+            dt.TableName = "EmployeeDetails";
+            dt.Columns.Add(new DataColumn("EmpId", typeof(string)));
+            dt.Columns.Add(new DataColumn("EmpName", typeof(string)));
+            dt.Columns.Add(new DataColumn("DeptName", typeof(string)));
+            dt.Columns.Add(new DataColumn("EmpAddress", typeof(string)));
+            dt.Columns.Add(new DataColumn("EmpSalary", typeof(string)));
+            dr = dt.NewRow();
+            dt.Rows.Add(dr);
+            //saving databale into viewstate   
+            ViewState["EmployeeDetails"] = dt;
+            //bind Gridview   
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+        private void AddNewRecordRowToGrid()
+        {
+            // check view state is not null   
+            if (ViewState["EmployeeDetails"] != null)
+            {
+                //get datatable from view state   
+                DataTable dtCurrentTable = (DataTable)ViewState["EmployeeDetails"];
+                DataRow drCurrentRow = null;
+                if (dtCurrentTable.Rows.Count > 0)
+                {
+                    for (int i = 1; i <= dtCurrentTable.Rows.Count; i++)
+                    {
+                        //add each row into data table   
+                        drCurrentRow = dtCurrentTable.NewRow();
+                        drCurrentRow["EmpId"] = txtEmpId.Text;
+                        drCurrentRow["EmpName"] = txtEmpName.Text;
+                        drCurrentRow["DeptName"] = txtDeptName.Text;
+                        drCurrentRow["EmpAddress"] = txtEmpAddress.Text;
+                        drCurrentRow["EmpSalary"] = txtEmpSalary.Text;
+                    }
+                    //Remove initial blank row   
+                    if (dtCurrentTable.Rows[0][0].ToString() == "")
+                    {
+                        dtCurrentTable.Rows[0].Delete();
+                        dtCurrentTable.AcceptChanges();
+                    }
+                    //add created Rows into dataTable   
+                    dtCurrentTable.Rows.Add(drCurrentRow);
+                    //Bind Gridview with latest Row   
+                    GridView1.DataSource = dtCurrentTable;
+                    GridView1.DataBind();
+                }
+            }
+        }
+        protected void AddEmpDetails_Click(object sender, EventArgs e)
+        {
+            AddNewRecordRowToGrid();
+        }
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add(new DataColumn("EmpId", typeof(string)));
+            dt.Columns.Add(new DataColumn("EmpName", typeof(string)));
+            dt.Columns.Add(new DataColumn("DeptName", typeof(string)));
+            dt.Columns.Add(new DataColumn("EmpAddress", typeof(string)));
+            dt.Columns.Add(new DataColumn("EmpSalary", typeof(string)));
+            string Id = string.Empty;
+            string Name = string.Empty;
+            string DeptName = string.Empty;
+            string EmpAddress = string.Empty;
+            string EmpSalary = string.Empty;
+            for (int i = 0; i < GridView1.Rows.Count; i++)
+            {
+                GridViewRow row = (GridViewRow)GridView1.Rows[i];
+                if (i != e.RowIndex)
+                {
+                    Id = row.Cells[1].Text;
+                    Name = row.Cells[2].Text;
+                    DeptName = row.Cells[3].Text;
+                    EmpAddress = row.Cells[4].Text;
+                    EmpSalary = row.Cells[5].Text;
+                }
+                else
+                {
+                    Id = ((TextBox)row.Cells[1].Controls[0]).Text;
+                    Name = ((TextBox)row.Cells[2].Controls[0]).Text;
+                    DeptName = ((TextBox)row.Cells[3].Controls[0]).Text;
+                    EmpAddress = ((TextBox)row.Cells[4].Controls[0]).Text;
+                    EmpSalary = ((TextBox)row.Cells[5].Controls[0]).Text;
+                }
+                DataRow dr = dt.NewRow();
+                dr["EmpId"] = Id;
+                dr["EmpName"] = Name;
+                dr["DeptName"] = DeptName;
+                dr["EmpAddress"] = EmpAddress;
+                dr["EmpSalary"] = EmpSalary;
+                dt.Rows.Add(dr);
+            }
+            GridView1.EditIndex = -1;
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            {
+                DataTable dt = new DataTable();
+                dt.Columns.Add(new DataColumn("EmpId", typeof(string)));
+                dt.Columns.Add(new DataColumn("EmpName", typeof(string)));
+                dt.Columns.Add(new DataColumn("DeptName", typeof(string)));
+                dt.Columns.Add(new DataColumn("EmpAddress", typeof(string)));
+                dt.Columns.Add(new DataColumn("EmpSalary", typeof(string)));
+                string Id = string.Empty;
+                string Name = string.Empty;
+                string DeptName = string.Empty;
+                string EmpAddress = string.Empty;
+                string EmpSalary = string.Empty;
+                for (int i = 0; i < GridView1.Rows.Count; i++)
+                {
+                    GridViewRow row = (GridViewRow)GridView1.Rows[i];
+                    if (i != e.RowIndex)
+                    {
+                        Id = row.Cells[1].Text;
+                        Name = row.Cells[2].Text;
+                        DeptName = row.Cells[3].Text;
+                        EmpAddress = row.Cells[4].Text;
+                        EmpSalary = row.Cells[5].Text;
+                        DataRow dr = dt.NewRow();
+                        dr["EmpId"] = Id;
+                        dr["EmpName"] = Name;
+                        dr["DeptName"] = DeptName;
+                        dr["EmpAddress"] = EmpAddress;
+                        dr["EmpSalary"] = EmpSalary;
+                        dt.Rows.Add(dr);
+                    }
+                }
+                GridView1.EditIndex = -1;
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            BindEmpDetails();
+        }
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            BindEmpDetails();
+        }
+        private void BindEmpDetails()
+        {
+            if (ViewState["EmployeeDetails"] != null)
+            {
+                //get datatable from view state   
+                DataTable dtCurrentTable = (DataTable)ViewState["EmployeeDetails"];
+                DataRow drCurrentRow = null;
+                if (dtCurrentTable.Rows.Count > 0)
+                {
+                    for (int i = 1; i <= dtCurrentTable.Rows.Count; i++)
+                    {
+                        //add each row into data table   
+                        drCurrentRow = dtCurrentTable.NewRow();
+                        drCurrentRow["EmpId"] = txtEmpId.Text;
+                        drCurrentRow["EmpName"] = txtDeptName.Text;
+                        drCurrentRow["DeptName"] = txtDeptName.Text;
+                        drCurrentRow["EmpAddress"] = txtEmpAddress.Text;
+                        drCurrentRow["EmpSalary"] = txtEmpSalary.Text;
+                    }
+                    //Remove initial blank row   
+                    if (dtCurrentTable.Rows[0][0].ToString() == "")
+                    {
+                        dtCurrentTable.Rows[0].Delete();
+                        dtCurrentTable.AcceptChanges();
+                    }
+                    //Bind Gridview with latest Row   
+                    GridView1.DataSource = dtCurrentTable;
+                    GridView1.DataBind();
+                }
+            }
+        }
+    }
+}
