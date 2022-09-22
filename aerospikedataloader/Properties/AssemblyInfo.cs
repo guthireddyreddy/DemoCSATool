5cbad60377d83f3b9a3e719707d6c536ddc54c96
Guthireddy Reddy <guthireddy.reddy@mphasis.com>
2022-09-22 12:58:31 +0530

    Dataloader with DotNet 4.8

---
+++ aerospikedataloader/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("aerospikedataloader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mphasis")]
+[assembly: AssemblyProduct("aerospikedataloader")]
+[assembly: AssemblyCopyright("Copyright © Mphasis 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3d02a2d8-158b-473f-9792-44789b2d3165")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ aerospikedataloader/program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using Aerospike;
+using Aerospike.Client;
+using MySql.Data.MySqlClient.Memcached;
+
+namespace aerospikedataloader
+{
+    public class program
+    {
+        static void Main(string[] args)
+
+        {
+            AerospikeClient client = new AerospikeClient("127.0.0.1", 3000);
+            XmlTextReader xtr = new XmlTextReader("Replace with XML File Location Path ");
+            string s1 = string.Empty;
+            string s2 = string.Empty;
+
+
+            while (xtr.Read())
+            {
+                if (xtr.NodeType == XmlNodeType.Element && xtr.Name == "name")
+                {
+                    s1 = xtr.ReadElementContentAsString();
+
+                }
+                if (xtr.NodeType == XmlNodeType.Element && xtr.Name == "class")
+                {
+                    s2 = xtr.ReadElementContentAsString();
+
+
+                }
+            }
+            Key key = new Key("test", "myset", "mykey");
+            Bin bin1 = new Bin("name", s1);
+            Bin bin2 = new Bin("name", s2);
+            Record record = client.Get(null, key);
+            client.Close();
+        }
+
+    }
+}
